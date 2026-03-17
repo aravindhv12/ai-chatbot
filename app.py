32c99551243eb0ddaae9d90f6d0999aa72201b36
aravindhv12 <aravindhan.v1208@gmail.com>
2026-03-17 15:28:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import tempfile
 # LangChain imports
 from langchain_groq import ChatGroq
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain.chains import RetrievalQA
